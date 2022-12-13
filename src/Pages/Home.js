ac62b1b3fc98ceee2affda0976702b9d9b47cfe2
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-12-13 23:23:11 +0600

    Redux-Content-corner is initiated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,20 +4,19 @@ const Home = () => {
     const [click, setClick] = useState(false);
 
 
-    const activeClass = "text-white bg-indigo-500 border-white";
+    const activeClass = "text-white bg-amber-700 border font-semibold";
     return (
         <div className='max-w-7xl gap-14 mx-auto my-10'>
             <div className='mb-10 flex justify-end gap-5'>
                 <button
                     onClick={() => setClick(true)}
-                    className={`border px-3 py-2 rounded-full font-semibold ${click ? activeClass : null
-                        } `}
+                    className={`border shadow-md px-3 py-2 rounded-lg font-thin ${click ? activeClass : null}`}
                 >
                     Last Upload
                 </button>
                 <button
                     onClick={() => setClick(false)}
-                    className={`border px-3 py-2 rounded-full font-semibold ${!click ? activeClass : null}`}
+                    className={`border shadow-md px-3 py-2 rounded-lg font-thin ${!click ? activeClass : null}`}
                 >
                     First Upload
                 </button>
